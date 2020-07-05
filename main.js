cf8562b10ba851b0a7490fb967aa6f9f4ad07d39
leonanastasia <leon.anastasia93@gmail.com>
2020-07-06 00:23:53 +0300

    added check for a winning combination

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const playerTurnTitle = document.querySelector(".player-turn")
 const cells = document.querySelectorAll(".game-cell");
 const cellsValues = [0,0,0,0,0,0,0,0,0];
 
-console.log('cells', cells);
-
 const makeMove = function(e){
     const currentCellIndex = e.target.classList[1].split('').reverse()[0] - 1;
     console.log(currentCellIndex);
     cells[currentCellIndex].classList.toggle(currentPlayer);
     cellsValues[currentCellIndex]=currentPlayer;
-   changePlayer()
+    if(checkWin()){
+        let winner = currentPlayer === player1 ? 'Player1':'Player2';
+        playerTurnTitle.innerHTML = winner+"WIN!"
+    }
+    else{changePlayer()}
     console.log('cellsValues', cellsValues);
 }
 const changePlayer = function(){
@@ -21,9 +23,36 @@ const changePlayer = function(){
         playerTurnTitle.innerHTML="Player's 2 turn!"
     }
     else{currentPlayer = player1; playerTurnTitle.innerHTML="Player's 1 turn!"}
+
 }
 
 for(var i = 0; i < cells.length; i++){
     cells[i].addEventListener("click", makeMove)
 }
 
+const checkWin = function(){
+    if(cellsValues[0] === currentPlayer && cellsValues[1] === currentPlayer && cellsValues[2] === currentPlayer){
+        return true
+    }
+    if(cellsValues[3] === currentPlayer && cellsValues[4] === currentPlayer && cellsValues[5] === currentPlayer){
+        return true
+    }
+    if(cellsValues[6] === currentPlayer && cellsValues[7] === currentPlayer && cellsValues[8] === currentPlayer){
+        return true
+    }
+    if(cellsValues[0] === currentPlayer && cellsValues[3] === currentPlayer && cellsValues[6] === currentPlayer){
+        return true
+    }
+    if(cellsValues[1] === currentPlayer && cellsValues[4] === currentPlayer && cellsValues[7] === currentPlayer){
+        return true
+    }
+    if(cellsValues[2] === currentPlayer && cellsValues[5] === currentPlayer && cellsValues[8] === currentPlayer){
+        return true
+    }
+    if(cellsValues[0] === currentPlayer && cellsValues[4] === currentPlayer && cellsValues[8] === currentPlayer){
+        return true
+    }
+    if(cellsValues[6] === currentPlayer && cellsValues[4] === currentPlayer && cellsValues[2] === currentPlayer){
+        return true
+    }else return false
+}
