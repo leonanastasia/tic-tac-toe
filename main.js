2167060d312b26978c2ed6e2f292e0ee13fe2c64
leonanastasia <leon.anastasia93@gmail.com>
2020-07-05 22:52:06 +0300

    added player's turn change

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-const player1 = "x";
-const player2 = "o";
-
+const player1 = "cross";
+const player2 = "zero";
+let currentPlayer = player1
+const playerTurnTitle = document.querySelector(".player-turn")
 const cells = document.querySelectorAll(".game-cell");
 const cellsValues = [0,0,0,0,0,0,0,0,0];
 
-
 console.log('cells', cells);
 
-const toggleCell = function(e){
+const makeMove = function(e){
     const currentCellIndex = e.target.classList[1].split('').reverse()[0] - 1;
     console.log(currentCellIndex);
-    cells[currentCellIndex].classList.toggle("cross");
-    cellsValues[currentCellIndex]='cross';
-
+    cells[currentCellIndex].classList.toggle(currentPlayer);
+    cellsValues[currentCellIndex]=currentPlayer;
+   changePlayer()
     console.log('cellsValues', cellsValues);
+}
+const changePlayer = function(){
+    if(currentPlayer === player1){
+        currentPlayer = player2;
+        playerTurnTitle.innerHTML="Player's 2 turn!"
+    }
+    else{currentPlayer = player1; playerTurnTitle.innerHTML="Player's 1 turn!"}
 }
 
 for(var i = 0; i < cells.length; i++){
-    cells[i].addEventListener("click", toggleCell)
+    cells[i].addEventListener("click", makeMove)
 }
+
